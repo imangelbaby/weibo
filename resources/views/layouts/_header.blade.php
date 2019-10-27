958157899f43d91517aa3c688743762c8283f758
531046244@qq.com <531046244@qq.com>
2019-10-27 17:27:37 +0800

    Move user model to models folder

--- resources/views/layouts/_header.blade.php
+++ resources/views/layouts/_header.blade.php
@@ -1,6 +1,6 @@
 <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
       <div class="container">
-        <a class="navbar-brand" href="{{ url('home') }}">Weibo App</a>
+        <a class="navbar-brand" href="{{ url('/') }}">Weibo App</a>
         <ul class="navbar-nav justify-content-end">
           <li class="nav-item"><a class="nav-link" href="{{ url('help') }}">帮助</a></li>
           <li class="nav-item" ><a class="nav-link" href="{{ url('signup') }}">现在注册</a></li>
