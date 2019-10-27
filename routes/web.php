6cbb488123c44816347ec69b49f78e4b367fb53d
531046244@qq.com <531046244@qq.com>
2019-10-27 20:44:07 +0800

    sx

--- routes/web.php
+++ routes/web.php
@@ -14,13 +14,5 @@ Route::get('/', 'StaticPagesController@home')->name('home');
 Route::get('/help', 'StaticPagesController@help')->name('help');
 Route::get('/about', 'StaticPagesController@about')->name('about');
 
-<<<<<<< HEAD
-Route::get('/', 'StaticPagesController@home')->name('home');
-Route::get('/help', 'StaticPagesController@help')->name('help');
-Route::get('/about', 'StaticPagesController@about')->name('about');
-
 Route::get('signup', 'UsersController@create')->name('signup');
 Route::resource('users', 'UsersController');
-=======
-Route::get('/signup', 'UsersController@create')->name('signup');
->>>>>>> filling-layout-style
