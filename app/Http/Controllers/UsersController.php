b17a0a4c8bc8249b402a9660f430b66aa0a5f1d5
531046244@qq.com <531046244@qq.com>
2019-10-27 21:29:51 +0800

    用户显示页面

--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -1,16 +1,25 @@
 <?php
-
 namespace App\Http\Controllers;
-
 use Illuminate\Http\Request;
+use App\Models\User;
+
 
 class UsersController extends Controller
 {
+
+    public function index(){
+        return view('users.index');
+    }
     /**
      * 注册
      * @return [type] [description]
      */
     public function create(){
     	return view('users.create');
+    }
+
+    public function show(User $user)
+    {
+        return view('users.show',compact('user'));
     }
 }
