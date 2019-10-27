50776e2a04493bd22f6219916a0c49d658d6a376
531046244@qq.com <531046244@qq.com>
2019-10-27 16:38:07 +0800

    用户注册页面

--- resources/views/layouts/_header.blade.php
+++ resources/views/layouts/_header.blade.php
@@ -3,7 +3,7 @@
         <a class="navbar-brand" href="{{ url('home') }}">Weibo App</a>
         <ul class="navbar-nav justify-content-end">
           <li class="nav-item"><a class="nav-link" href="{{ url('help') }}">帮助</a></li>
-          <li class="nav-item" ><a class="nav-link" href="{{ url('login' }})">登录</a></li>
+          <li class="nav-item" ><a class="nav-link" href="{{ url('signup') }}">现在注册</a></li>
         </ul>
       </div>
     </nav>
