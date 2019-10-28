b8323c0c3a757c5a867bc57f9cd426e0794719b0
531046244@qq.com <531046244@qq.com>
2019-10-28 09:48:58 +0800

    创建会话

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,9 @@ Route::get('/about', 'StaticPagesController@about')->name('about');
 
 Route::get('signup', 'UsersController@create')->name('signup');
 Route::resource('users', 'UsersController');
+
+
+//login
+Route::get('login', 'SessionsController@create')->name('login');
+Route::post('login', 'SessionsController@store')->name('login');
+Route::delete('logout', 'SessionsController@destroy')->name('logout');
