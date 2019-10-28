364b65499a5850230760330279902eec048b6dae
531046244@qq.com <531046244@qq.com>
2019-10-28 16:45:18 +0800

    管理员删除用户

--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -81,6 +81,11 @@ class UsersController extends Controller
         session()->flash('success','个人资料更新成功');
         return redirect()->route('users.show',[$user->id]);
     }
-
+    //删除用户
+    public function destroy(User $user){
+        $user->delete();
+        session()->flash('success', '成功删除用户！');
+        return back();
+    }
 
 }
