b30382976eae99719ee206504f3947ec9ec9b6fa
531046244@qq.com <531046244@qq.com>
2019-10-29 22:03:10 +0800

    社交统计信息

--- app/Models/User.php
+++ app/Models/User.php
@@ -90,7 +90,6 @@ class User extends Authenticatable
         }
         $this->followings()->detach($user_ids);
     }
-
     public function isFollowing($user_id)
     {
         return $this->followings->contains($user_id);
